b296912622f7af6d737481640e2fe61aff7be161
e.redun <e.redun@mail.ru>
2026-08-16 19:34:18 +0500

    DataViewer.UI.Avalonia - Microsoft.Extensions.DependencyInjection - nuget

--- DataViewer.UI.Avalonia/App.axaml.cs
+++ DataViewer.UI.Avalonia/App.axaml.cs
@@ -1,11 +1,16 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Common.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace DataViewer.UI.Avalonia;
 
 public partial class App : Application
 {
+    public static IServiceProvider Services { get; private set; } = null!;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -13,9 +18,22 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddTransient<MainWindowModel>();
+
+        Services = serviceCollection.BuildServiceProvider();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            var mainWindowModel = Services.GetRequiredService<MainWindowModel>();
+
+            var mainWindow = new MainWindow();
+
+            mainWindowModel.CloseRequest += () => mainWindow.Close();
+
+            mainWindow.DataContext = mainWindowModel;
+            desktop.MainWindow = mainWindow;
         }
 
         base.OnFrameworkInitializationCompleted();
