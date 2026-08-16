49fca090940bb50e86d9d737bfc098ef029c86d2
e.redun <e.redun@mail.ru>
2026-08-16 11:22:26 +0500

    Microsoft.Extensions.DependencyInjection - nuget; Common - lib

---
+++ Common/ViewModels/MainWindowModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ViewModels
+{
+    public class MainWindowModel
+    {
+        public event Action? RequestClose;
+
+        //[RelayCommand]
+        private void CloseApplication()
+        {
+            // Вместо вызова метода конкретного окна, просто генерируем событие
+            RequestClose?.Invoke();
+        }
+
+    }
+}
--- DataViewer.UI.WPF/App.xaml.cs
+++ DataViewer.UI.WPF/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Common.ViewModels;
+using DataViewer.UI.Wpf.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -6,9 +9,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace DataViewer
+namespace DataViewer.UI.Wpf
 {
     public partial class App : Application
     {
+        public static IServiceProvider Services { get; private set; }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+
+            services.AddTransient<MainWindowModel>();
+
+            Services = services.BuildServiceProvider();
+
+            var mainWindow = new MainWindow();
+            var mainWindowModel = Services.GetRequiredService<MainWindowModel>();
+
+            mainWindowModel.RequestClose += () => mainWindow.Close();
+            mainWindow.DataContext = mainWindowModel;
+
+            mainWindow.Show();
+        }
     }
 }
