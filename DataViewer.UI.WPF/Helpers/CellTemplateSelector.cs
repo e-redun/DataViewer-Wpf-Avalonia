f3c9be149e2a7b129f31753da2680d1fcde53576
e.redun <e.redun@mail.ru>
2026-08-18 21:15:41 +0500

    CellTemplates.Instance.TestTemplate

---
+++ DataViewer.UI.WPF/Helpers/CellTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using DataViewer.UI.Wpf.Templates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DataViewer.UI.Wpf.Helpers
+{
+    internal class CellTemplateSelector : DataTemplateSelector
+    {
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return CellTemplates.Instance.TestTemplate;
+        }
+    }
+}
---
+++ DataViewer.UI.WPF/Templates/CellTemplates.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace DataViewer.UI.Wpf.Templates
+{
+    public partial class CellTemplates
+    {
+        internal static readonly CellTemplates Instance = new CellTemplates();
+        public DataTemplate TestTemplate => (DataTemplate)this["TestTemplate"];
+
+
+        public CellTemplates()
+        {
+            InitializeComponent();
+        }
+    }
+}
