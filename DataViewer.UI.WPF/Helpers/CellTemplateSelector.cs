5e9ac8283410531e335eec0eb243e42ed105a015
e.redun <e.redun@mail.ru>
2026-08-18 22:50:44 +0500

    Wpf.PropertiesView - список полей со значениями

--- DataViewer.UI.WPF/Helpers/CellTemplateSelector.cs
+++ DataViewer.UI.WPF/Helpers/CellTemplateSelector.cs
@@ -1,7 +1,4 @@
 ﻿using DataViewer.UI.Wpf.Templates;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,7 +6,6 @@ namespace DataViewer.UI.Wpf.Helpers
 {
     internal class CellTemplateSelector : DataTemplateSelector
     {
-
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
             return CellTemplates.Instance.TestTemplate;
