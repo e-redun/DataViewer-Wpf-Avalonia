ee126a9952c96c90dc7ca022e8613be569048bd1
e.redun <e.redun@mail.ru>
2026-08-19 15:10:16 +0500

    Авалония собирается

---
+++ DataViewer.UI.Avalonia/Views/DataBasesView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DataViewer.UI.Avalonia.Views
+{
+    public partial class DataBasesView : UserControl
+    {
+        public DataBasesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
