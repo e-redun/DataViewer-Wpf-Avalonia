035d1820dc74b7ec01f4b74b6d952c7664898b9c
e.redun <e.redun@mail.ru>
2026-08-19 15:41:09 +0500

    Шаблоны работают -вар 1

--- DataViewer.UI.Avalonia/Views/DataBasesView.axaml.cs
+++ DataViewer.UI.Avalonia/Views/DataBasesView.axaml.cs
@@ -5,7 +5,7 @@ using Avalonia.Markup.Xaml;
 namespace DataViewer.UI.Avalonia.Views
 {
     public partial class DataBasesView : UserControl
-    {
+    { 
         public DataBasesView()
         {
             InitializeComponent();
